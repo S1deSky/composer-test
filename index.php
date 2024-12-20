33feb5e3bdac1d03aea2b2462d268fa4e9c963cd
SideSky <sideskyofficial@gmail.com>
2024-12-20 16:02:57 +0100

    test docroot

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once "vendor/autoload.php";
 
 $client = new \Contentful\Delivery\Client('QO1APueMQ8xDusZW0n9LqgFsJwYzr4ietKlqo1SBIGA', 'hmai60ni45fc');
 
+echo $_SERVER["DOCUMENT_ROOT"];
 
-
-$entries = $client->getEntries();
-echo $entries->getTotal();
+// $entries = $client->getEntries();
+// echo $entries->getTotal();
