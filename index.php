be1ce0a37eb01290407bfbd77fc464eb020939f4
SideSky <sideskyofficial@gmail.com>
2024-12-21 17:44:28 +0100

    Format & echo SERVER_NAME too

--- index.php
+++ index.php
@@ -3,7 +3,10 @@ require_once "vendor/autoload.php";
 
 $client = new \Contentful\Delivery\Client('QO1APueMQ8xDusZW0n9LqgFsJwYzr4ietKlqo1SBIGA', 'hmai60ni45fc');
 
-echo $_SERVER["HTTP_HOST"]; 
+echo "HTTP_HOST: " . $_SERVER["HTTP_HOST"]; 
+echo "<br>";
+echo "SERVER_NAME: " . $_SERVER["SERVER_NAME"]; 
+
 
 // $entries = $client->getEntries();
 // echo $entries->getTotal();
