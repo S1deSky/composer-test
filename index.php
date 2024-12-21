7dbc7093ce46dc61353d787d83a044160a338329
SideSky <sideskyofficial@gmail.com>
2024-12-21 17:42:41 +0100

    Echo HTTP_HOST only

--- index.php
+++ index.php
@@ -3,8 +3,7 @@ require_once "vendor/autoload.php";
 
 $client = new \Contentful\Delivery\Client('QO1APueMQ8xDusZW0n9LqgFsJwYzr4ietKlqo1SBIGA', 'hmai60ni45fc');
 
-echo $_SERVER["HTTP_HOST"];
-echo $_SERVER["DOCUMENT_ROOT"];
+echo $_SERVER["HTTP_HOST"]; 
 
 // $entries = $client->getEntries();
 // echo $entries->getTotal();
