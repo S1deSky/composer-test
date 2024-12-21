110a960ae4351c6b910a6a6d27ffb8987c4fa104
SideSky <sideskyofficial@gmail.com>
2024-12-21 17:41:51 +0100

    Echo HTTP_HOST

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once "vendor/autoload.php";
 
 $client = new \Contentful\Delivery\Client('QO1APueMQ8xDusZW0n9LqgFsJwYzr4ietKlqo1SBIGA', 'hmai60ni45fc');
 
+echo $_SERVER["HTTP_HOST"];
 echo $_SERVER["DOCUMENT_ROOT"];
 
 // $entries = $client->getEntries();
